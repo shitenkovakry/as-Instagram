3d33edd6a3a3ac2b6f2091c3b625b6de918e18c7
kry shitenkova <shitenkovakris@mail.ru>
2023-05-05 19:20:58 +0300

    fix my mistake in models

--- models/photos/photo.go
+++ models/photos/photo.go
@@ -3,7 +3,7 @@ package photo
 type Photo struct {
 	IDPhoto   int `bson:"id_photo"`
 	IDUser    int `bson:"id_user"`
-	IdComment int `bson::id_comment"`
+	IdComment int `bson:"id_comment"`
 }
 
 type Photos []*Photo
